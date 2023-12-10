b35f09d86d75c0ffc3f9412fcd03ed6b8ff401d5
Logic Monkey <logic.monkey@protonmail.com>
2023-12-10 14:03:14 -0600

    Control bleep doodad, so that controls bleep when selected and chime when used.

---
+++ control_bleeper.gd
@@ -0,0 +1,41 @@
+extends AudioStreamPlayer2D
+class_name ControlBleeper
+
+var blip = preload("blip.wav")
+var select = preload("select.wav")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	bus = "effects input"
+	volume_db = linear_to_db(0.5)
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	
+	await get_tree().process_frame
+	
+	var bb = load("res://blackboard.tres")
+	if bb:
+		if "control_highlight_sound" in bb and bb.control_highlight_sound:
+			blip = bb.control_highlight_sound
+		if "control_activate_sound" in bb and bb.control_activate_sound:
+			select = bb.control_activate_sound
+	
+	var c = get_parent() as Control
+	if not c: return
+	global_position = c.get_global_rect().get_center()
+		
+	c.focus_entered.connect(_on_owner_highlight)
+	c.mouse_entered.connect(_on_owner_highlight)
+	var b = c as BaseButton
+	if b:
+		c.pressed.connect(_on_owner_pressed)
+
+func _on_owner_highlight():
+	stop()
+	stream = blip
+	play()
+
+func _on_owner_pressed():
+	stop()
+	stream = select
+	play()
